2f7665d9c93bbb57a6946e8ce2b85c166ce654e6
VitorConteCesul <vitor.2024014338@alunocesul.com>
2025-04-03 22:05:02 -0300

    Adionando Main

--- 2°ano/Projetos/Venda/src/Carrinho.java
+++ 2°ano/Projetos/Venda/src/Carrinho.java
@@ -1,3 +1,4 @@
+import java.security.cert.PolicyNode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,6 +7,10 @@ public class Carrinho {
 
     public Carrinho(){
         itens = new ArrayList<>();
+    }
+
+    public List<Produto> getItens(){
+        return itens;
     }
 
     public void adicionarProduto(Produto p){
@@ -19,6 +24,36 @@ public class Carrinho {
         }else {
             System.out.println("Produto não removido");
         }
+    }
 
+    public double calcularTotal(){
+        double total = 0.0;
+        for (Produto p : itens){
+            total += p.getPreco() + p.calcularFrete();
+        }
+        return total;
+    }
+
+    // Aplicar desconto somente em itens que implementem Promocional
+
+    public void aplicarDescontoEmPromocionais(double porcentagem){
+        //iterar sobre todos os itens da lista
+        // verificar quais implementam a interface Promocional
+
+        for(Produto p : itens){
+            if(p instanceof Promocional){
+                Promocional promo = (Promocional) p;
+                promo.aplicarDesconto(porcentagem);
+            }else {
+                System.out.println("Produto não é promocional");
+            }
+        }
+    }
+
+    public void listarItens(){
+        System.out.println("Itens no carrinho");
+        for (Produto p : itens){
+            p.exibirInfo();
+        }
     }
 }
--- 2°ano/Projetos/Venda/src/Main.java
+++ 2°ano/Projetos/Venda/src/Main.java
@@ -1,5 +1,116 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        Carrinho carrinho = new Carrinho();
+        int opcao = -1;
+
+        while(opcao != 0){
+            System.out.println("-- Loja --");
+            System.out.println("1 - Adicionar Produto");
+            System.out.println("2 - Remover Produto");
+            System.out.println("3 - Listar itens do Carrinho");
+            System.out.println("4 - Aplicar desconto a produtos prom.");
+            System.out.println("5 - Calcular Total (preço + frete)");
+            System.out.println("0 - Sair");
+            System.out.println("Escolha uma opção: ");
+            opcao = Integer.parseInt(sc.nextLine());
+
+            switch (opcao){
+                case 1:
+                    adicionarProdutoMenu(sc, carrinho);
+                    break;
+                case 2:
+                    removerProdutoMenu(sc, carrinho);
+                    break;
+
+                case 3:
+                    carrinho.listarItens();
+                    break;
+
+                case 4:
+                    aplicarDescontoMenu(sc, carrinho);
+
+                case 5:
+                    double total = carrinho.calcularTotal();
+                    System.out.println("Valor total do carrinho: R$ " + total);
+                    break;
+
+                case 0:
+                    System.out.println("Saindo do sistema...");
+
+                default:
+                    System.out.println("Opção Inválida. Digite outra opção: ");
+
+            }
+        }
+    }
+
+    private static void adicionarProdutoMenu(Scanner sc, Carrinho carrinho){
+        System.out.println("\n -- Adicionar produto --");
+        System.out.println("Qual é o tipo do produto que deseja adicionar");
+        System.out.println("1 - Eletronico");
+        System.out.println("2 - Roupa");
+        System.out.println("3 - Livro");
+        System.out.println("Digite a opção:");
+        int tipo = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Digite o nome do produto: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite o preço do produto: ");
+        double preco = Double.parseDouble(sc.nextLine());
+
+        switch(tipo){
+            case 1:
+                System.out.println("Digite a marca do eletronico: ");
+                String marca = sc.nextLine();
+                Eletronico e = new Eletronico(nome, preco, marca);
+                carrinho.adicionarProduto(e);
+                break;
+            case 2:
+                System.out.println("Digite o tamanho da roupa: ");
+                String tamanho = sc.nextLine();
+                Roupa r = new Roupa(nome, preco, tamanho);
+                carrinho.adicionarProduto(r);
+                break;
+            case 3:
+                System.out.println("Digite o autor do livro: ");
+                String autor = sc.nextLine();
+                Livro l = new Livro(nome, preco, autor);
+                carrinho.adicionarProduto(l);
+                break;
+            default:
+                System.out.println("Tipo invalido, produto nao adicionado");
+        }
+    }
+
+    private static void removerProdutoMenu(Scanner sc, Carrinho carrinho){
+        System.out.println("\n -- Remover produto --");
+        System.out.println("Qual o nome do produto que deseja remover");
+        String nomeRemover = (sc.nextLine());
+
+        Produto produtoEncontrado = null;
+
+        for (Produto p : carrinho.getItens()){
+            if (p.getNome().equals(nomeRemover)){
+                carrinho.removerProduto(p);
+                produtoEncontrado = p;
+            }
+        }
+
+        if (produtoEncontrado != null){
+            carrinho.removerProduto(produtoEncontrado);
+        }
+        System.out.println("Produto não encontrado");
+    }
+
+    private static void aplicarDescontoMenu(Scanner sc, Carrinho carrinho){
+        System.out.println("\n ---Aplicar Desconto --");
+        System.out.println("Digite a porcentagem de desconto ");
+        double porcentagem = Double.parseDouble(sc.nextLine());
+
+        carrinho.aplicarDescontoEmPromocionais(porcentagem / 100);
     }
 }
