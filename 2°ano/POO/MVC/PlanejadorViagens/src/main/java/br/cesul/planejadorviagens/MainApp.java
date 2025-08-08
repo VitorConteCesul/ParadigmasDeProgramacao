6a5e6b05017e264e48387b929cfc44d96b1854ce
VitorConteCesul <vitor.2024014338@alunocesul.com>
2025-08-07 22:32:13 -0300

    adicionando

--- 2°ano/POO/MVC/PlanejadorViagens/src/main/java/br/cesul/planejadorviagens/MainApp.java
+++ 2°ano/POO/MVC/PlanejadorViagens/src/main/java/br/cesul/planejadorviagens/MainApp.java
@@ -1,13 +1,22 @@
 package br.cesul.planejadorviagens;
 
-/**
- * Hello world!
- *
- */
-public class MainApp
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception{
+        var loader = new FXMLLoader(getClass().getResource("/br.cesul.planejadorviagens/trip_view.fxml"));
+
+        Parent root = loader.load();
+
+        stage.setTitle("Trip Planner - MVC");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
+
+    public static void main( String[] args ) {launch(args);}
 }
--- 2°ano/POO/MVC/PlanejadorViagens/src/main/java/br/cesul/planejadorviagens/controller/TripController.java
+++ 2°ano/POO/MVC/PlanejadorViagens/src/main/java/br/cesul/planejadorviagens/controller/TripController.java
@@ -12,8 +12,14 @@ package br.cesul.planejadorviagens.controller;
 // com os novos dados vindos do service
 // Responde a eventos da view
 
+import br.cesul.planejadorviagens.model.Viagem;
+import br.cesul.planejadorviagens.services.PlanejamentoService;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.time.format.DateTimeFormatter;
 
 public class TripController {
     // Elementos do FXML (fx:id)
@@ -23,10 +29,62 @@ public class TripController {
     @FXML private TextField destinoField;
     @FXML private TextField orcamentoField;
     @FXML private Button btnAdicionar;
-    @FXML private TableView viagensTable;
-    @FXML private TableColumn colCidade;
-    @FXML private TableColumn ColIni;
-    @FXML private TableColumn ColFim;
-    @FXML private TableColumn colCusto;
+    @FXML private TableView<Viagem> viagensTable;
+    @FXML private TableColumn<Viagem, String> colCidade;
+    @FXML private TableColumn<Viagem, String> colIni;
+    @FXML private TableColumn<Viagem, String> colFim;
+    @FXML private TableColumn<Viagem, Number> colCusto;
     @FXML private Label lbtTotal;
+
+    private final PlanejamentoService service = new PlanejamentoService();
+
+    private final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    @FXML
+    private void initialize(){
+        // Define COMO cada coluna extrai informação da entidade
+
+        // Se só remover uma String simples, o JFX não consegue
+        // saber se o valor mudou depois. Por isso se devolver uma
+        // SimplesStringProperty ele pode observar aquela propriedade
+        // e atualizar a interface automaticamente.
+
+        colCidade.setCellValueFactory(c ->
+                new SimpleStringProperty(c.getValue().getDestino()));
+
+        colIni.setCellFactory(c ->
+                new SimpleStringProperty(c.getValue().getDataInicio().format(fmt)));
+
+        colFim.setCellFactory(c ->
+                new SimpleStringProperty(c.getValue().getDataFim().format(fmt)));
+
+        colCusto.setCellFactory(c ->
+                new SimpleStringProperty(c.getValue().getCusto()));
+
+        // Preencher a tabela com dados já gravados
+        // Chamando o service para buscar as viagens
+        viagensTable.setItems(FXCollections.observableArrayList(service.listar()));
+        atualizarTotal();
+    }
+
+    @FXML
+    public void adicionar(){
+        // Handler do botão adicionar viagem
+
+        try{
+            // Conversão de dados
+            double custo = Double.parseDouble(orcamentoField.getText().replace(",", "."));
+        }catch (Exception ex){
+            mostrarErro(ex.getMessage());
+        }
+
+    }
+
+    private void mostrarErro(String msg){
+        new Alert(Alert.AlertType.ERROR, msg).showAndWait();
+    }
+
+    private void atualizarTotal(){
+        lbtTotal.setText("Total: R$ " + String.format("%.2f",service.totalGasto()));
+    }
 }
